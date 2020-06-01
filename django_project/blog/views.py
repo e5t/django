b2e5da42b487b28ba0427faddf00b7d5b96345f5
jason <e5t@yahoo.com>
2020-06-01 14:47:48 -0600

    Added about

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -3,4 +3,9 @@ from django.http import HttpResponse
 
 def home(request):
     return HttpResponse('<h1>Blog Home</h1>')
-# Create your views here.
+
+
+def about(request):
+    return HttpResponse('<h1>Blog About</h1>')
+
+    
